5eeb8a23105476762f1d2596f62e7b3993be0978
David Andrews <dandrewsuk82@gmail.com>
2019-11-19 09:20:29 +0000

    initial commit

---
+++ main.js
@@ -0,0 +1,71 @@
+const { app, BrowserWindow, ipcMain, dialog } = require("electron");
+const fs = require("fs");
+const path = require("path");
+// const sharp = require("sharp");
+const Jimp = require("jimp");
+const PDFDocument = require("pdfkit");
+const uniqid = require("uniqid");
+
+// Hot reload!
+require("electron-reload")(__dirname);
+
+const createWindow = () => {
+  let win = new BrowserWindow({
+    // fullscreen: true,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+  win.loadFile("./index.html");
+
+  ipcMain.on("choose image", (e, arg) => {
+    const id = uniqid();
+    const fpath = dialog.showOpenDialogSync(win, {
+      title: "Choose an image....",
+      filters: [
+        {
+          name: "Images",
+          extensions: ["jpg", "png", "JPG"]
+        }
+      ]
+    });
+    if (!fs.existsSync(path.join(__dirname, "/.cache/"))) {
+      fs.mkdirSync("./.cache");
+    }
+
+    Jimp.read(fpath[0], (err, img) => {
+      if (err) throw err;
+      img
+        .cover(350, 350)
+        .quality(100)
+        // .normalize()
+        .color([
+          // {apply: 'desaturate', params: [25]}
+          { apply: "blue", params: [25] }
+        ])
+        .write(`.cache/${id}.jpg`, () => {
+          win.webContents.send(
+            "filepath",
+            path.join(__dirname, `.cache/${id}.jpg`)
+          );
+        });
+    });
+  });
+  ipcMain.on("test", (e, arg) => {
+    const doc = new PDFDocument();
+    doc.pipe(fs.createWriteStream("test.pdf"));
+    doc.text(arg);
+    doc.end();
+    // win.webContents.send("something");
+  });
+};
+
+app.on("ready", createWindow);
+app.on("will-quit", e => {
+  e.preventDefault();
+  const cacheDir = fs.readdirSync(".cache");
+  cacheDir.forEach(element => {
+    fs.unlinkSync(`.cache/${element}`);
+  });
+  app.quit()
+});
